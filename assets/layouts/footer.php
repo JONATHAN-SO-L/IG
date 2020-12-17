ed1f5e76699fe0e0838da8ed24e037557c08da49
JONATHAN-SO-L <sanceznarval00@outlook.com>
2020-12-16 20:07:47 -0600

    Se cambia imagen de link al dominio

--- assets/layouts/footer.php
+++ assets/layouts/footer.php
@@ -3,7 +3,7 @@
 <footer>
 	
 	<div class="container" align="center">
-		<a style="margin: 20px" href="https://my.jw.org/home/es" target="_blank"><img id="apps" src="https://assets1.jw.org/images/siteMarkApps_mobile.svg">DOMINIO DE LA CONGREGACIÓN</a>
+		<a style="margin: 20px" href="https://my.jw.org/home/es" target="_blank"><img id="apps" src="https://cdh.azureedge.net/assets/cdh/branding/v1/apple-touch-icon.png">DOMINIO DE LA CONGREGACIÓN</a>
 		<p id="foot">All rights reserved. Copyrights by Jonathán Sánchez ©</p>
 		<p id="foot">Versión 1.0.0</p>
 		<p id="foot">Last release 2021</p>
